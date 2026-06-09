bde8880e70a1c3748c1ca8bb69374478f37a5c03
Abhilash A B <abhilashab461@gmail.com>
2026-06-09 23:32:27 +0530

    updated code for deploying

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+
 const api = axios.create({
 
     baseURL:
-        "http://localhost:8000"
+        import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use(
